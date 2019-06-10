fbef62543cee45bab2adc22170f1a98558a597d4
noorosce <077068@yahoo.com>
2019-06-10 18:46:32 -0500

    Project has been completed.

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,20 +5,41 @@
 // };
 // myFunction();
 
+
+// ** My code starts here **
+let myFunction = () => console.log("Function was invoked!");
+// myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+
+// ** My code starts here **
+let anotherFunction = (param) =>  param;
+// console.log(anotherFunction("Example"));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+
+// ** My code starts here **
+let add = (param1, param2) => param1 + param2;
+console.log(add(1,1));
+
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+
+// ** My code starts here **
+let subtract = (param1, param2) => param1 - param2;
+console.log(subtract(5,2));
 
 
 // Stretch
